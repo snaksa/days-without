bfd5fee5c42fee60730be8b5d17283bf72a039b0
snaksa <sinan.zikri@gmail.com>
2024-11-10 21:43:33 +0200

    Add additional categories

--- lib/helpers/category_helper.dart
+++ lib/helpers/category_helper.dart
@@ -18,6 +18,14 @@ class CategoryHelper {
     Category(12, 'Social Networks', Icons.phone_android, Colors.amber),
     Category(14, 'Adult Entertainment', Icons.live_tv, Colors.orange),
     Category(15, 'Intimacy', Icons.favorite, Colors.red),
+    Category(16, 'OCD', Icons.track_changes, Colors.indigo),
+    Category(17, 'Procrastination', Icons.schedule, Colors.purple),
+    Category(18, 'Meat', Icons.food_bank, Colors.redAccent),
+    Category(19, 'Dairy', Icons.opacity, Colors.blueAccent),
+    Category(20, 'Swearing', Icons.record_voice_over, Colors.teal),
+    Category(21, 'Sinning', Icons.self_improvement, Colors.orangeAccent),
+    Category(22, 'Abuse', Icons.shield, Colors.deepOrange),
+    Category(23, 'Anger', Icons.sentiment_dissatisfied, Colors.red),
     Category(13, 'Other', Icons.autorenew, Colors.cyan),
   ];
 
